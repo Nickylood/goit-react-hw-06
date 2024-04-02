ec0f6c19f01125aacc3d5329cff468b5c8ce8139
Dmytro Tkach <dreamworkshow@gmail.com>
2024-04-02 23:58:14 +0200

    hw-6 fix-componentsFix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,20 @@
 import css from "./App.module.css";
-import ContactForm from "../companents/ContactForm/ContactForm.jsx";
-import ContactList from "../companents/ContactList/ContactList.jsx";
-import SearchBox from "../companents/SearchBox/SearchBox.jsx";
+import ContactForm from "../ContactForm/ContactForm";
+import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
 import { useSelector, useDispatch } from "react-redux";
 import {
   selectContacts,
   addContact,
   deleteContact,
 } from "../../redux/contactsSlice.js";
-import { selectNameFilter, changeFilter } from "../../redux/filtersSlice.js";
+import { selectNameFilter, changeFilter } from "../../redux/filtersSlice";
 
 export default function App() {
   const contacts = useSelector(selectContacts);
+
   const filter = useSelector(selectNameFilter);
+
   const dispatch = useDispatch();
 
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-import "./index.css";
 import "modern-normalize";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
